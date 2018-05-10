a5c977494bba2baa2b82d8fec65090a59b802136
Molly N <mollykathrynnelson@gmail.com>
2018-05-10 00:58:23 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,4 +1,4 @@
 INSERT INTO bears VALUES (1, "Mr. Chocolate", 2, "M", "brown", "shy", 1);
-INSERT INTO bears VALUES (1, "Rowdy", 11, "M", "tan", "rowdy", 1);
-INSERT INTO bears VALUES (1, "Tabitha", 22, "M", "black", "sassy", 0);
-INSERT INTO bears VALUES (1, "Rowdy", 11, "F", "tan", "rowdy", 1);
+INSERT INTO bears VALUES (2, "Rowdy", 11, "M", "tan", "rowdy", 1);
+INSERT INTO bears VALUES (3, "Tabitha", 22, "M", "black", "sassy", 0);
+INSERT INTO bears VALUES (4, "Rowdy", 11, "F", "tan", "rowdy", 1);
