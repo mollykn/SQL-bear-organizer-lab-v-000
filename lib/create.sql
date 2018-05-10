a872e5b92031ffcf85a8f540fbe349b746c64fce
Molly N <mollykathrynnelson@gmail.com>
2018-05-10 00:51:29 +0000

    automatically backed up by learn

--- lib/create.sql
+++ lib/create.sql
@@ -1,1 +1,1 @@
-CREATE TABLE bears (id INTEGER PRIMARY KEY, name TEXT, age INTEGER, gender CHAR(1), color TEXT, temperment TEXT, alive BOOLEAN);
+CREATE TABLE bears (id INTEGER PRIMARY KEY, name TEXT, age INTEGER, gender CHAR(1), color TEXT, temperament TEXT, alive BOOLEAN);
